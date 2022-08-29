8b80578087af0475e0dcc711e6561c0d300c400e
HIANDIC <leaands25@gmail.com>
2022-08-29 08:24:40 +0200

    modify hello-app.py for web hosting

--- flask-01/hello-app.py
+++ flask-01/hello-app.py
@@ -25,6 +25,9 @@ def forth(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # for local
+    # app.run(debug=True)
+
     # for hosting web not local
-    #app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=80)
